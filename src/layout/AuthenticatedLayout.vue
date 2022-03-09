e27ca29674b601f6bfe8b43b1247577306b7388a
Yago dos Santos Rocha <yrocha@braspag.com.br>
2022-03-09 12:25:39 -0300

    fixes

--- src/layout/AuthenticatedLayout.vue
+++ src/layout/AuthenticatedLayout.vue
@@ -2,7 +2,7 @@
   <div>
     <app-sidenav></app-sidenav>
     <main
-      class="main-content position-relative max-height-vh-100 h-100 border-radius-lg "
+      class="main-content overflow-auto position-relative max-height-vh-100 h-100 border-radius-lg "
     >
       <app-navbar />
       <div class="container-fluid py-4">
