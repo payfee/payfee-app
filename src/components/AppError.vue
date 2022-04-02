52677140d1cde2a7329acc130c00e2a111f04faf
Yago dos Santos Rocha <yrocha@braspag.com.br>
2022-04-02 19:22:58 -0300

    fixing api error

--- src/components/AppError.vue
+++ src/components/AppError.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="alert alert-danger text-white">
-    Erro ao realizar operação
+	<div class="d-block text-center">
+    <div class="mx-auto alert d-inline-block p-4 alert-danger bg-danger-soft text-danger">
+		<payfee-icon icon="fad fa-triangle-exclamation" class="mx-auto d-block mb-4 fs-1" />
+		Erro ao realizar operação
+	</div>
   </div>
 </template>
 
